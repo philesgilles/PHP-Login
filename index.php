0c7ec6c2352cfb4915eb79791dd5672226a69cdf
Philippe Gilles <philgdep@gmail.com>
2019-10-23 10:25:50 +0200

    beautyfull beaaach

--- index.php
+++ index.php
@@ -15,25 +15,36 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
 <body>
 
     <?php include 'components/navbar.php'?>
+    <div class="background">
+        <div class="water"></div>
+    </div>
+    <svg>
+        <filter id="turbulence" x="0" y="0" width="100%" height="100%">
+            <feTurbulence id="sea-filter" numOctaves="3" seed="2" baseFrequency="0.02 0.05"></feTurbulence>
+            <feDisplacementMap scale="20" in="SourceGraphic"></feDisplacementMap>
+            <animate xlink:href="#sea-filter" attributeName="baseFrequency" dur="60s" keyTimes="0;0.5;1"
+                values="0.02 0.06;0.04 0.08;0.02 0.06" repeatCount="indefinite" />
+        </filter>
+    </svg>
     <div class="container">
-        <div class="page-header">
-            <h1>Hi,
-                <b><?php echo htmlspecialchars(!empty($_SESSION['firstname'])) ? ($_SESSION["firstname"]) : ($_SESSION["username"]); ?></b>.
+        <div class="alert alert-info" role="alert">
+            <h1>Hi,<b><?php echo htmlspecialchars(!empty($_SESSION['firstname'])) ? ($_SESSION["firstname"]) : ($_SESSION["username"]); ?></b>.
                 did you know that :</h1>
+            <p id="joke" class="lead">
+            </p>
+            <div class="d-flex justify-content-center">
+                <button id="newJoke" class="btn aqua-gradient waves-effect">Learn something else</button>
             </div>
-            <div class="alert alert-info" role="alert">
-                <p id="joke" class="lead">
-                    </p>
-                    <button id="newJoke" class="btn btn-sm aqua-gradient waves-effect">Learn something else</button>
-                </div>
-            <?php if (empty($_SESSION['firstname'])): ?>
-                <div class="alert alert-warning mt-3" role="alert">
-                    <p class="text-center">You havent completed your profile. please <a class="btn-link" href="/user_reg/auth/account.php">click here</a> to update your informations
-                    </p>
-                </div>
-            <?php endif;?>
-            </div>
-            <script>
+        </div>
+        <?php if (empty($_SESSION['firstname'])): ?>
+        <div class="alert alert-warning mt-3" role="alert">
+            <p class="text-center">You havent completed your profile. please <a class="btn-link"
+                    href="/user_reg/auth/account.php">click here</a> to update your informations
+            </p>
+        </div>
+        <?php endif;?>
+    </div>
+    <script>
     getUserAsync = async (url) => {
         let response = await fetch(url);
         let data = await response.json()
